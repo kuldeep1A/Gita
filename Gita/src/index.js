ec356b9916172e012cfdc0dc403c71515fb5eca6
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2024-03-24 19:18:40 +0530

    remove last webpack update

--- Gita/src/index.js
+++ Gita/src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import "./style/C4AQA02wjGYD0MZL1EIoKn.css";
-import "./style/C5MO0Ki3krNT2TII4f56T2.css";
-import "./style/normalization/safwijfdsklfi2owjlskjfowijlfjsl.css";
+import App from "./App";
 
-// Assuming you have an element with id "root" in your HTML
-const root = document.getElementById("root");
-
-// Render your React app inside the root element
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  root,
 );
