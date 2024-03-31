2641337e622af674c843b37ed9c0ca95b0042cf3
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2024-03-31 22:17:01 +0530

    update the original gita site link

--- Gita/main.js
+++ Gita/main.js
@@ -1,20 +1,20 @@
-import gita1aw from "/gita1aw.jpg";
-import gita1ab from "/gita1ab.jpg";
-import "./style.css";
-import { isDark } from "./isDark";
+import gita1aw from '/gita1aw.jpg';
+import gita1ab from '/gita1ab.jpg';
+import './style.css';
+import { isDark } from './isDark';
 
-document.querySelector("#app").innerHTML = `
+document.querySelector('#app').innerHTML = `
   <div class='gc'>
 ${
   isDark()
-    ? `<a href="#" target=""> 
+    ? `<a href="https://gitas.web.app/" target=""> 
     <div  class='gc1'></div><img src="${gita1ab}" class="logo" alt="gita1a white" /></a>`
-    : `<a href="#" target="">
+    : `<a href="https://gitas.web.app/" target="">
     <div  class='gc1'></div><img src="${gita1aw}" class="logo" alt="gita1a black" /></a>`
 }   
     <div class='gc2'> <h1>Hello Gita User's</h1>
     <p class="Info">
-    Now Available Soon,<br>
+    Now its Available<br> at <br><br><br><strong><a href="https://gitas.web.app/" target=""> gitas.web.app</a></strong><br><br><br>
       Gita Link Updation...
     </p> </div>
   </div>
@@ -23,4 +23,3 @@ ${
 `;
 
 isDark();
-
